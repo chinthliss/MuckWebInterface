bfb1656e34227dba48715ed1722641a04cdfad61
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2022-10-20 16:04:45 +0100

    Bring across MuckInterOp

--- config/app.php
+++ config/app.php
@@ -209,7 +209,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'ExampleClass' => App\Example\ExampleClass::class,
+        'MuckInterop' => App\Helpers\MuckInterop::class,
     ])->toArray(),
 
 ];
