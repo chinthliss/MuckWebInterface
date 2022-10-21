aa3596f1270a86dfda6444d183aaf57b7b78f4c9
chinthliss <55845146+chinthliss@users.noreply.github.com>
2022-10-21 01:18:52 +0100

    Add content to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,30 @@
 @extends('layout.page-no-navigation')
 
 @section('content')
-    Welcome Test
+    <div class="d-flex flex-column">
+        <div class="mx-auto">
+            <h1>Welcome</h1>
+            <p>This is a work-in-progress new site. At some point this whole welcome will be replaced.</p>
+        </div>
+        <div class="d-flex justify-content-evenly">
+            <div>
+                <h2 class="mt-2">Singleplayer</h2>
+                <p>Here is where we'll put more information about accessing singleplayer.</p>
+                <a class="btn btn-primary" href="{{ route('singleplayer.home') }}">
+                    <!-- TODO reimplement icons: <i class="fas fa-user btn-icon-left"></i> -->
+                    Enter Singleplayer
+                </a>
+            </div>
+            <div>
+                <h2 class="mt-2">Multiplayer</h2>
+                <p>Here is where we'll put more information about accessing multiplayer.</p>
+                <a class="btn btn-primary" href="{{ route('multiplayer.home') }}">
+                    <!-- TODO reimplement icons: <i class="fas fa-users btn-icon-left"></i> -->
+                    Enter Multiplayer
+                </a>
+            </div>
+        </div>
+    </div>
 @endsection
 
 
