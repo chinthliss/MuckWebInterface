205fa0dff024f5abbd90f9f0225607a4c7f22e4a
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2022-10-27 15:46:47 +0100

    Add seeded users for development

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\TermsOfService;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use MuckInterop;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,13 +16,54 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // *************************************
+        // Regular User Account - user@test.com
+        $accountId = 1;
+        DB::table('accounts')->insert([
+            'aid' => $accountId,
+            'uuid' => 'fake-uuid-1',
+            'email' => 'user@test.com',
+            'password' => MuckInterop::createSHA1SALTPassword('password'),
+            'password_type' => 'SHA1SALT'
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('account_emails')->insert([
+            'aid' => $accountId,
+            'email' => 'user@test.com',
+            'verified_at' => Carbon::now()
+        ]);
+
+        DB::table('account_properties')->insert([
+            'aid' => $accountId,
+            'propname' => 'tos-hash-viewed',
+            'proptype' => 'STRING',
+            'propdata' => TermsOfService::getTermsOfServiceHash()
+        ]);
+
+        // *************************************
+        // Admin Account - admin@test.com
+        $accountId = 2;
+        DB::table('accounts')->insert([
+            'aid' => $accountId,
+            'uuid' => 'fake-uuid-2',
+            'email' => 'admin@test.com',
+            'password' => MuckInterop::createSHA1SALTPassword('password'),
+            'password_type' => 'SHA1SALT'
+        ]);
+
+        DB::table('account_emails')->insert([
+            'aid' => $accountId,
+            'email' => 'admin@test.com',
+            'verified_at' => Carbon::now()
+        ]);
+
+        DB::table('account_properties')->insert([
+            'aid' => $accountId,
+            'propname' => 'tos-hash-viewed',
+            'proptype' => 'STRING',
+            'propdata' => TermsOfService::getTermsOfServiceHash()
+        ]);
     }
 }
