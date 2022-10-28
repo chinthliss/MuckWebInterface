14c17e83bd5caff39f3f0073c58dabccf6b88a57
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2022-10-28 16:49:49 +0100

    Add missing content to terms of service page

--- resources/views/auth/terms-of-service.blade.php
+++ resources/views/auth/terms-of-service.blade.php
@@ -3,5 +3,27 @@
 @section('title', 'Terms of Service')
 
 @section('content')
-    Terms of Service
+    <div class="container">
+        <div>
+            @foreach ($termsOfService as $line)
+                {{ $line }} <br/>
+            @endforeach
+        </div>
+        @auth
+            @if ($agreed)
+                <div class="p-2 mb-2 bg-primary text-dark">You've agreed to this previously.</div>
+            @else
+                <div class="border border-primary rounded p-3 text-center">
+                    <form action="{{ route('auth.terms-of-service') }}" method="POST">
+                        @csrf
+                        <input type="hidden" name="_hash" value="{{ $hash }}">
+
+                        <button type="submit" value="submit" class="btn btn-primary">
+                            Click here to agree to the Terms of Service
+                        </button>
+                    </form>
+                </div>
+            @endif
+        @endauth
+    </div>
 @endsection
