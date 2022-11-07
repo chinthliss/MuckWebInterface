62ac543abd2a6787735651c88975a6ccb9f6b82c
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2022-11-07 14:09:49 +0000

    Switch to database session driver

---
+++ database/migrations/2022_11_07_140439_create_sessions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->text('payload');
+            $table->integer('last_activity')->index();
+            //TODO Later - investigate live db, which has aid instead of user_id and different indexes
+            //$table->unsignedBigInteger('aid')->nullable()->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        throw new Error("Can not reverse this migration. Use 'artisan migrate:fresh --seed' for testing.");
+    }
+};
