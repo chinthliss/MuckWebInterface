ecf58160ab4c13dfcd072502ab0bced596b932f4
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2022-11-08 17:10:27 +0000

    Fix capital to handle blank strings

--- resources/js/formatting.js
+++ resources/js/formatting.js
@@ -8,6 +8,7 @@ export const carbonToString = (carbonString) => {
 };
 
 export const capital = (text) => {
+    if (!text) return;
     if (typeof (text) !== 'string') text = text.toString();
     return text && text.substring(0, 1).toUpperCase() + text.slice(1);
 };
