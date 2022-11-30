e2735ef52491e19faaec5f4bddac8b924ae6b698
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2022-11-30 13:08:42 +0000

    refactor: rewrote MuckConnectionFaker to be more consistent

--- app/Muck/MuckConnectionFaker.php
+++ app/Muck/MuckConnectionFaker.php
@@ -9,22 +9,59 @@ use Database\Seeders\DatabaseSeeder;
 
 class MuckConnectionFaker implements MuckConnection
 {
+    /**
+     * Collection of fixed fake data to run tests against.
+     * Late loaded to avoid it being populated for every test.
+     * @var MuckDbref[]
+     */
+    private array $muckDatabase = [];
+
+    private function populateDatabaseIfRequired()
+    {
+        if (count($this->muckDatabase)) return;
+        $this->muckDatabase[] = new MuckDbref(1234, 'TestCharacter', 'p', Carbon::now(), [
+            'accountId' => strval(DatabaseSeeder::$normalUserAccountId),
+            'level' => '10',
+            'approved' => '1'
+        ]);
+        $this->muckDatabase[] = new MuckDbref(1235, 'TestAltCharacter', 'p', Carbon::now(), [
+            'accountId' => strval(DatabaseSeeder::$normalUserAccountId),
+            'level' => '2',
+            'approved' => '1'
+        ]);
+        $this->muckDatabase[] = new MuckDbref(1235, 'unapprovedCharacter', 'p', Carbon::now(), [
+            'accountId' => strval(DatabaseSeeder::$normalUserAccountId),
+            'level' => '1'
+        ]);
+
+    }
+
+    // Converts a dbref into the string the muck would return, in order to test parsing of such.
+    private function dbrefToMuckResponse(MuckDbref $dbref): string
+    {
+        // Format - dbref,creationTimestamp,typeFlag,"name", .. otherProperties
+        $piecesArray = [$dbref->dbref, $dbref->createdTimestamp->getTimestamp(), $dbref->typeFlag, $dbref->name];
+        if ($dbref->accountId()) $piecesArray[] = "\"accountId=" . $dbref->accountId() . "\"";
+        if ($dbref->level()) $piecesArray[] = "\"level=" . $dbref->level() . "\"";
+        if ($dbref->approved()) $piecesArray[] = "\"approved=1\"";
+        return join(',', $piecesArray);
+    }
 
     public function fake_getByDbref(array $data): string
     {
-        throw new Error("Not implemented.");
+        $dbrefWanted = $data['dbref'];
+        foreach ($this->muckDatabase as $dbref) {
+            if ($dbref->dbref == $dbrefWanted) return $this->dbrefToMuckResponse($dbref);
+        }
+        return '';
     }
 
     public function fake_getByPlayerName(array $data): string
     {
-        // Format - dbref,creationTimestamp,typeFlag,"name", .. otherProperties
-        $characterArray = null;
-        switch ($data['name']) {
-            case 'TestCharacter':
-                $characterArray = [1234, Carbon::now()->getTimestamp(), 'p', 'TestCharacter', '"accountId=' . DatabaseSeeder::$normalUserAccountId . '"'];
-                break;
+        $nameWanted = $data['name'];
+        foreach ($this->muckDatabase as $dbref) {
+            if ($dbref->name == $nameWanted) return $this->dbrefToMuckResponse($dbref);
         }
-        if ($characterArray) return join(',', $characterArray);
         return '';
     }
 
@@ -35,15 +72,13 @@ class MuckConnectionFaker implements MuckConnection
 
     public function fake_getCharacters(array $data): string
     {
+        $accountWanted = $data['aid'];
         $characters = [];
-        switch ($data['aid']) {
-            case '1':
-                $characters = [
-                    [1234, Carbon::now()->getTimestamp(), 'p', 'TestCharacter', '"accountId=' . DatabaseSeeder::$normalUserAccountId . '"']
-                ];
+        foreach ($this->muckDatabase as $dbref) {
+            if ($dbref->accountId() == $accountWanted) $characters[] = $dbref;
         }
-        return  join(chr(13) . chr(10), array_map(function($characterArray) {
-            return join(',', $characterArray);
+        return join(chr(13) . chr(10), array_map(function ($character) {
+            return $this->dbrefToMuckResponse($character);
         }, $characters));
     }
 
@@ -55,6 +90,7 @@ class MuckConnectionFaker implements MuckConnection
     public function request(string $request, array $data = []): string
     {
         Log::debug('FakeMuckRequest:' . $request . ', request: ' . json_encode($data));
+        $this->populateDatabaseIfRequired();
 
         $fakerFunction = 'fake_' . $request;
         if (method_exists($this, $fakerFunction)) {
