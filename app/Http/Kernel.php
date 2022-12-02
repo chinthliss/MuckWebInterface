c50a8b2cfaa6ba37239d21ce2d704e123563a9c1
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2022-12-02 16:25:33 +0000

    fix: Forgot to commit updated kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Http;
 
-use App\Http\Middleware\PreserveActiveCharacter;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -22,7 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\PreserveActiveCharacter::class,
         \App\Http\Middleware\LogHost::class,
     ];
 
@@ -39,6 +37,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\LoadActiveCharacter::class,
+            \App\Http\Middleware\SaveActiveCharacter::class,
         ],
 
         'api' => [
