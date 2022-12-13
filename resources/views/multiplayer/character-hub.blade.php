85863d9e2095936c4304dfc75dc8be9daf16107e
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2022-12-13 17:25:26 +0000

    feat: Add some details and styling to edit character page

--- resources/views/multiplayer/character-hub.blade.php
+++ resources/views/multiplayer/character-hub.blade.php
@@ -11,18 +11,18 @@
 @endsection
 
 <!-- TODO: Profile and Inventory were initially listed here and need relocating -->
+<!-- TODO: Look at bringing across the things under the 'settings' page onto this page -->
 @php ($pages = [
-        ['title' => 'Avatar', 'description' => 'What does this do?', 'url' => route('multiplayer.character')],
-        ['title' => 'Settings', 'description' => 'What does this do?', 'url' => route('multiplayer.character')],
-        ['title' => 'Inventory', 'description' => 'What does this do?', 'url' => route('multiplayer.character')],
-        ['title' => 'Training', 'description' => 'What does this do?', 'url' => route('multiplayer.character')],
-        ['title' => 'Perks', 'description' => 'What does this do?', 'url' => route('multiplayer.character')],
-        ['title' => 'Quirks', 'description' => 'What does this do?', 'url' => route('multiplayer.character')],
-        ['title' => 'Skills', 'description' => 'What does this do?', 'url' => route('multiplayer.character')],
-        ['title' => 'Kinks', 'description' => 'What does this do?', 'url' => route('multiplayer.character')],
-        ['title' => 'Classes', 'description' => 'What does this do?', 'url' => route('multiplayer.character')],
-        ['title' => 'Dedication', 'description' => 'What does this do?', 'url' => route('multiplayer.character')],
-        ['title' => 'AI', 'description' => 'What does this do?', 'url' => route('multiplayer.character')],
+        ['title' => 'Avatar', 'description' => 'Customize the graphical representation of your character.', 'url' => route('multiplayer.character')],
+        ['title' => 'Perks', 'description' => 'Buy new perks that give (mostly) positive benefits.', 'url' => route('multiplayer.character')],
+        ['title' => 'Perk Notes', 'description' => 'Add optional information to your perks.', 'url' => route('multiplayer.character')],
+        ['title' => 'Quirks', 'description' => 'Buy different things that are essentially a different type of perk?', 'url' => route('multiplayer.character')],
+        ['title' => 'Classes', 'description' => 'Change your class.', 'url' => route('multiplayer.character')],
+        ['title' => 'Skills', 'description' => 'Select your professions. And shows some things that should be on the profile page.', 'url' => route('multiplayer.character')],
+        ['title' => 'Training', 'description' => 'Something about proficiencies? And upgrades some things.', 'url' => route('multiplayer.character')],
+        ['title' => 'Kinks', 'description' => 'Set your detailed preferences for interacting with other characters.', 'url' => route('multiplayer.character')],
+        ['title' => 'Dedication', 'description' => 'Get information about dedications or switch your current one. There\'s also some fake terminal output?', 'url' => route('multiplayer.character')],
+        ['title' => 'AI', 'description' => 'Adjust how your character acts in combat whilst under automatic control.', 'url' => route('multiplayer.character')],
 ])
 
 @section('content')
@@ -32,16 +32,18 @@
         </div>
         <div class="row">
             <p>This page acts as a hub for all the things you can edit on your character.</p>
-            <p>Base character settings can maybe go here, e.g. short description</p>
+            <p>TODO: Look at bringing across the other toggles on the settings page onto this page.</p>
+            <p>Base character settings go here, e.g. short description</p>
         </div>
-        <div class="row">
-            <div class="container">
-                <ul>
-                    @foreach ($pages as $page)
-                        <li>{{ $page['title'] }}</li>
-                    @endforeach
-                </ul>
-            </div>
+        <div class="row g-2">
+            @foreach ($pages as $page)
+                <div class="card col-6">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $page['title'] }}</h5>
+                        <div class="card-text">{{ $page['description'] }}</div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
