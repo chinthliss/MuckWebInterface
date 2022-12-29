84cc9695b59ebac6137e05ab1aa158372715f844
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2022-12-29 12:54:14 +0000

    style: Re-add FA icon to home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,16 +10,16 @@
             <div>
                 <h2 class="mt-2">Singleplayer</h2>
                 <p>Here is where we'll put more information about accessing singleplayer.</p>
-                <a class="btn btn-primary" href="{{ route('singleplayer.home') }}">
-                    <!-- TODO reimplement icons: <i class="fas fa-user btn-icon-left"></i> -->
+                <a class="btn btn-primary w-100" href="{{ route('singleplayer.home') }}">
+                    <i class="fas fa-user btn-icon-left"></i>
                     Enter Singleplayer
                 </a>
             </div>
             <div>
                 <h2 class="mt-2">Multiplayer</h2>
                 <p>Here is where we'll put more information about accessing multiplayer.</p>
-                <a class="btn btn-primary" href="{{ route('multiplayer.home') }}">
-                    <!-- TODO reimplement icons: <i class="fas fa-users btn-icon-left"></i> -->
+                <a class="btn btn-primary w-100" href="{{ route('multiplayer.home') }}">
+                    <i class="fas fa-users btn-icon-left"></i>
                     Enter Multiplayer
                 </a>
             </div>
