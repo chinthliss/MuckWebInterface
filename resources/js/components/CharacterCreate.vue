b48d112bdcbde3f0b521353b9d3cc0e2e7f15385
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2023-02-17 17:12:40 +0000

    fix: Type in getting-started

--- resources/js/components/CharacterCreate.vue
+++ resources/js/components/CharacterCreate.vue
@@ -3,7 +3,7 @@
         <h1>Create a new Character</h1>
         <div class="row">
             <div class="col-12 col-md-6">
-                <p>Please enter a name, it needs to be at least three letters long. A single hypen can be used in the name but no special characters.</p>
+                <p>Please enter a name, it needs to be at least three letters long. A single hyphen can be used in the name but no special characters, including diacritics.</p>
                 <p>This is supposed to be a reasonable name, such as what a person would go by in real life.</p>
                 <p>WARNING: Profane or copyrighted names will be changed without warning.</p>
             </div>
