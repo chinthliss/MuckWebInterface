735b5a3415e2842a153b61f716da0bfe879b2de8
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2023-04-21 13:18:30 +0100

    refactor: Cleanup CardManagementController.php

--- app/Http/Controllers/Payment/CardManagementController.php
+++ app/Http/Controllers/Payment/CardManagementController.php
@@ -7,19 +7,23 @@ use App\Payment\CardPaymentManager;
 use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Log;
+use Illuminate\View\View;
+use InvalidArgumentException;
+use Throwable;
 
 class CardManagementController extends Controller
 {
-    public function show(CardPaymentManager $cardPaymentManager)
+    public function show(CardPaymentManager $cardPaymentManager): View
     {
         /** @var User $user */
         $user = auth()->user();
 
         $cards = [];
         foreach ($cardPaymentManager->getCardsFor($user) as $card) {
-            array_push($cards, $card->toArray());
+            $cards[] = $card->toArray();
         }
 
         return view('auth.card-management', [
@@ -29,7 +33,10 @@ class CardManagementController extends Controller
         ]);
     }
 
-    public function addCard(Request $request, CardPaymentManager $cardPaymentManager)
+    /**
+     * @throws ValidationException
+     */
+    public function addCard(Request $request, CardPaymentManager $cardPaymentManager): Response
     {
         $cardNumber = $request['cardNumber'];
         $expiryDate = $request['expiryDate'];
@@ -41,16 +48,19 @@ class CardManagementController extends Controller
         $user = auth()->user();
         try {
             $card = $cardPaymentManager->createCardFor($user, $cardNumber, $expiryDate, $securityCode);
-        } catch (\InvalidArgumentException $e) {
-            throw ValidationException::withMessages(['cardNumber'=>'The given card was rejected by the authorization server.']);
-        } catch (\Throwable $e) {
+        } catch (InvalidArgumentException $e) {
+            throw ValidationException::withMessages(['cardNumber' => 'The given card was rejected by the authorization server.']);
+        } catch (Throwable $e) {
             Log::error(self::class . " addCard failure: " . $e->getMessage());
-            throw ValidationException::withMessages(['cardNumber'=>'An internal server error occurred. The actual error has been logged for staff to review.']);
+            throw ValidationException::withMessages(['cardNumber' => 'An internal server error occurred. The actual error has been logged for staff to review.']);
         }
         return response(json_encode($card->toArray()), 200);
     }
 
-    public function deleteCard(Request $request, CardPaymentManager $cardPaymentManager)
+    /**
+     * @throws ValidationException
+     */
+    public function deleteCard(Request $request, CardPaymentManager $cardPaymentManager): Response
     {
         if (!$request->has('id')) return response('Card ID missing', 400);
         $cardId = $request['id'];
@@ -60,14 +70,17 @@ class CardManagementController extends Controller
         try {
             $card = $cardPaymentManager->getCardFor($user, $cardId);
             $cardPaymentManager->deleteCardFor($user, $card);
-        } catch (\Throwable $e) {
+        } catch (Throwable $e) {
             Log::error($e->getMessage());
-            throw ValidationException::withMessages(['cardNumber'=>'An internal server error occurred. The actual error has been logged for staff to review.']);
+            throw ValidationException::withMessages(['cardNumber' => 'An internal server error occurred. The actual error has been logged for staff to review.']);
         }
         return response("OK", 200);
     }
 
-    public function setDefaultCard(Request $request, CardPaymentManager $cardPaymentManager)
+    /**
+     * @throws ValidationException
+     */
+    public function setDefaultCard(Request $request, CardPaymentManager $cardPaymentManager): Response
     {
         $cardId = $request['id'];
         if (!$cardId) return response('Card ID missing', 400);
@@ -76,9 +89,9 @@ class CardManagementController extends Controller
         try {
             $card = $cardPaymentManager->getCardFor($user, $cardId);
             $cardPaymentManager->setDefaultCardFor($user, $card);
-        } catch (\Throwable $e) {
+        } catch (Throwable $e) {
             Log::error($e->getMessage());
-            throw ValidationException::withMessages(['cardNumber'=>'An internal server error occurred. The actual error has been logged for staff to review.']);
+            throw ValidationException::withMessages(['cardNumber' => 'An internal server error occurred. The actual error has been logged for staff to review.']);
         }
         return response("OK", 200);
     }
