9ba1a13c34ad1b6b51e9af307782b4fbd13d8909
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2023-05-05 17:13:54 +0100

    refactor: HasRole complains if used out of order

--- app/Http/Middleware/HasRole.php
+++ app/Http/Middleware/HasRole.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\User as User;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class HasRole
 {
@@ -20,7 +21,13 @@ class HasRole
     {
         /** @var User $user */
         $user = $request->user();
-        if (!$user || !$user->hasRole($role)) { // hasRole deals with special exceptions
+
+        if (!$user) {
+            Log::Warn("HasRole was used on an incoming connection with no user. A previous middleware should be assured this.");
+            abort(403);
+        }
+
+        if (!$user->hasRole($role)) { // hasRole deals with special exceptions
             abort(403);
         }
         return $next($request);
