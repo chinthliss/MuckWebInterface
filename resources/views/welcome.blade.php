facc21c16cd232b67b7d4dea644b529d1c577e1f
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2023-06-13 16:19:18 +0100

    style: Tiny changes to welcome screen to fix padding

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,21 +2,21 @@
 
 @section('content')
     <div class="d-flex flex-column">
-        <div class="mx-auto">
+        <div class="p-2">
             <h1>Welcome</h1>
             <p>This is a work-in-progress new site. At some point this whole welcome will be replaced.</p>
         </div>
         <div class="d-flex justify-content-evenly">
-            <div>
-                <h2 class="mt-2">Singleplayer</h2>
+            <div class="p-2">
+                <h2>Singleplayer</h2>
                 <p>Here is where we'll put more information about accessing singleplayer.</p>
                 <a class="btn btn-primary w-100" href="{{ route('singleplayer.home') }}">
                     <i class="fas fa-user"></i>
                     Enter Singleplayer
                 </a>
             </div>
-            <div>
-                <h2 class="mt-2">Multiplayer</h2>
+            <div class="p-2">
+                <h2>Multiplayer</h2>
                 <p>Here is where we'll put more information about accessing multiplayer.</p>
                 <a class="btn btn-primary w-100" href="{{ route('multiplayer.home') }}">
                     <i class="fas fa-users"></i>
