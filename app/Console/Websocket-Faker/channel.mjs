4d19c68cc419a35ce0b3c3ee3ae4b337237108c6
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2023-07-17 17:20:44 +0100

    feat: Continued work on websocket emulator

--- app/Console/Websocket-Faker/channel.mjs
+++ app/Console/Websocket-Faker/channel.mjs
@@ -7,13 +7,47 @@ export default class Channel {
 
     /**
      * Connections we're aware of
-     * @type Map
+     * @type WebSocket[]
      */
     #connections
 
     constructor(channelName) {
         if (typeof channelName !== 'string' || channelName === '') throw "Attempt to create channel with no channelName specified.";
         this.#name = channelName;
-        this.#connections = new Map();
+        this.#connections = [];
     }
+
+    #transmitMessageToConnection = (connection, message, data) => {
+        const parsedMessage = 'MSG' + this.#name + ',' + message + ',' + (data ? JSON.stringify(data) : '');
+        console.log(" >> " + parsedMessage);
+        connection.send(parsedMessage + '\r\n');
+    }
+
+    sendMessageToConnection = (connection, message, data) => {
+        this.#transmitMessageToConnection(connection, message, data);
+    }
+
+    sendMessageToChannel = (message, data) => {
+        for (const connection of this.#connections) {
+            this.#transmitMessageToConnection(connection, message, data);
+        }
+    }
+
+    disconnect = (connection) => {
+        this.#connections.filter((value) => {
+            return (value !== connection);
+        })
+    }
+
+    connect = (connection) => {
+        this.#connections.push(connection);
+        this.sendMessageToConnection(connection, 'connected', 1)
+        this.sendMessageToConnection(connection, 'playerConnected', 1)
+        this.sendMessageToConnection(connection, 'accountConnected', 1)
+    }
+
+    messageReceived = (connection, message, data) => {
+
+    }
+
 }
--- app/Console/Websocket-Faker/index.mjs
+++ app/Console/Websocket-Faker/index.mjs
@@ -1,52 +1,65 @@
 /**
- * This is an ugly muck websocket emulator to allow local development
+ * This is an ugly muck websocket emulator to allow local development.
  */
 import { WebSocketServer } from 'ws';
+import Channel from "./channel.mjs";
 
 console.log("Creating the server.");
 const server = new WebSocketServer({ port: 8001 });
 
+/**
+ * @type {Map<WebSocket, Array>}
+ */
 const connections = new Map();
 
-// Every line from the muck ends with \r\n so we have to fake this
-const expectedLineEnding = String.fromCharCode(13) + String.fromCharCode(10);
-
-// Mostly a utility function to ensure the message is logged and has the line ending
-const transmitMessageToConnection = (connection, message) => {
-    console.debug(" >> " + message);
-    connection.send(message + expectedLineEnding);
-}
-
-const sendChannelMessage = (connection, channel, message, data) => {
-    transmitMessageToConnection('MSG' + channel + ',' + message + ',' + (data ? JSON.stringify(data) : '') );
-}
-
-const sendSystemMessage = (connection, message, data) => {
-    transmitMessageToConnection('SYS' + message + ',' + (data ? JSON.stringify(data) : '') );
-}
+/**
+ * @type {Map<string, Channel>}
+ */
+const channels = new Map();
 
 const processIncomingMessage = (connection, unprocessedMessage) => {
-    let [channel, message, data] = unprocessedMessage.split(',', 3);
-    console.log(" << " + channel + "." + message + ": " + data);
+    const [channelName, message, data] = unprocessedMessage.split(',', 3);
+    console.log(" << " + channelName + "." + message + ": " + data);
+    const channel = this.channels[channelName];
+    channel.messageReceived(connection, message, data);
 }
 
 const processIncomingSystemMessage = (connection, unprocessedMessage) => {
     let [message, data] = unprocessedMessage.split(',', 2);
     console.log(" << SYS." + message + ": " + data);
+    switch (message) {
+        case 'joinChannels':
+            let channelsToJoin = JSON.parse(data);
+            if (typeof(channelsToJoin) === 'string') channelsToJoin = [channelsToJoin];
+            for (const channel of channelsToJoin) {
+                if (!channels.has(channel)) {
+                    console.log("Creating channel: " + channel);
+                    channels[channel] = new Channel(channel);
+                }
+                channels[channel].connect(connection);
+                console.log(`Connection joined channel '${channel}'`);
+            }
+            break;
+        case 'test':
+            console.log("Mwi-Websocket Test message received. Data=", data);
+            break;
+        default:
+            console.error("Unrecognized system message received: " + message);
+    }
 }
 
 server.on('connection', (connection, request) => {
     console.log("Connection - new from: ", request.socket.remoteAddress);
     const data = {};
     connections.set(connection, data);
-    transmitMessageToConnection(connection, 'welcome');
+    connection.send('welcome\r\n');
 
     connection.on('message', (data) => {
-        console.debug(" << %s", data);
+        //console.debug(" << %s", data);
         const message = data.toString();
 
         if (message.startsWith('auth FAKEWEBSOCKETAUTHTOKEN')) {
-            transmitMessageToConnection(connection, 'accepted 1,1234,Test');
+            connection.send('accepted 1,1234,Test');
         }
 
         if (message.startsWith('MSG')) processIncomingMessage(connection, message.slice(3));
@@ -57,6 +70,9 @@ server.on('connection', (connection, request) => {
 
     connection.on('close', () => {
         console.log("Connection - dropped from: ", request.socket.remoteAddress);
+        for (const [, channel] of channels) {
+            channel.disconnect(connection);
+        }
         connections.delete(connection);
     });
 
