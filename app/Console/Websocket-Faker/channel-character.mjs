173d44dec94012f04f5e0a9747abb83bd1ef4bc2
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2023-07-20 17:10:18 +0100

    Scope out start on character profiles

--- app/Console/Websocket-Faker/channel-character.mjs
+++ app/Console/Websocket-Faker/channel-character.mjs
@@ -1,4 +1,27 @@
 import Channel from './channel.mjs';
 export default class ChannelCharacter extends Channel {
 
+    messageReceived = (connection, message, data) => {
+        switch (message) {
+            case 'getCharacterProfile':
+                const characterName = data.characterName;
+                const profile = {
+                    name: characterName,
+                    sex: '',
+                    species: '',
+                    shortDescription: '',
+                    faction: '',
+                    group: '',
+                    height: '',
+                    role: '',
+                    whatIs: '',
+                    equipment: ''
+                };
+                this.sendMessageToConnection(connection, 'characterProfile', profile);
+                break;
+            default:
+                console.log("Unhandled message: ", message);
+        }
+    }
+
 }
