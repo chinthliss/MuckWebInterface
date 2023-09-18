27076db88190647ded58fc59e4758cb99f02bc46
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2023-09-18 15:36:59 +0100

    chore: Add note to remember some singleplayer content

--- resources/views/singleplayer/home.blade.php
+++ resources/views/singleplayer/home.blade.php
@@ -12,6 +12,8 @@
 @section('content')
     <div class="container">
         TODO: Singleplayer home content
+
+        Most likely a link to the blog but consider moving or having a copy of 'how to download/play' here.
     </div>
 @endsection
 
