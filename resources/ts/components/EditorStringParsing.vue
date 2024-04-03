54415dc19ff40de9cc8b49af66bf171315049e0d
chinthliss <55845146+chinthliss@users.noreply.github.com>
2024-04-03 22:15:25 +0100

    style: Separate out 'control' and 'lookup' formatting in stringparsing

--- resources/ts/components/EditorStringParsing.vue
+++ resources/ts/components/EditorStringParsing.vue
@@ -7,20 +7,21 @@ import {highlightSelectionMatches, searchKeymap} from "@codemirror/search"
 import {autocompletion, completionKeymap} from "@codemirror/autocomplete"
 
 import {stringParsing} from "codemirror-lang-stringparsing";
-import {tags} from "@lezer/highlight"
+import {tags as t} from "@lezer/highlight"
 import {onMounted} from "vue";
 
 const emit = defineEmits(['update'])
 
 const highlightStyle = HighlightStyle.define(
     [
-        {tag: tags.controlKeyword, color: "#7b87b8"},
-        {tag: tags.processingInstruction, color: "#fb87b8"},
-        {tag: tags.arithmeticOperator, color: "#ab87b8"},
-        {tag: tags.comment, color: "#585858"},
-        {tag: tags.number, color: "#0b67b8"},
-        {tag: tags.variableName, color: "#0bb867"},
-        {tag: tags.compareOperator, color: "#b8b80b"}
+        { tag: t.keyword, color: "#7b87b8" },
+        { tag: t.controlKeyword, color: "#f8835c" },
+        { tag: t.processingInstruction, color: "#8b00ff" },
+        { tag: t.arithmeticOperator, color: "#ab87b8" },
+        { tag: t.comment, color: "#585858" },
+        { tag: t.number, color: "#0b67b8" },
+        { tag: t.variableName, color: "#0bb867" },
+        { tag: t.compareOperator, color: "#b8b80b" }
     ],
     {all: {color: "#989898"}}
 );
