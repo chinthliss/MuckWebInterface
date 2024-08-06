536eb37421eaa4e7fde7cad3d3c32fe9349f696b
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2024-08-06 16:55:19 +0100

    style: Also drop decimal places from percentage

--- resources/ts/components/Progress.vue
+++ resources/ts/components/Progress.vue
@@ -9,7 +9,7 @@ const props = defineProps<{
     <div class="progress" role="progressbar" aria-label="{{ alt }}"
          aria-valuemin="0" aria-valuemax="100" :aria-valuenow="percentage"
     >
-        <div v-if="percentage > 0" class="progress-bar" :style="'width: ' + percentage + '%'">{{ percentage }}%</div>
+        <div v-if="percentage > 0" class="progress-bar" :style="'width: ' + Math.floor(percentage) + '%'">{{ percentage }}%</div>
     </div>
 </template>
 
