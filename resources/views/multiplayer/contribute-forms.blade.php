c0c36766eba8ccca0434eb2e268b73008486321d
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2025-01-31 15:45:37 +0000

    content: Update tasks list for formeditor

--- resources/views/multiplayer/contribute-forms.blade.php
+++ resources/views/multiplayer/contribute-forms.blade.php
@@ -34,17 +34,17 @@
         ></form-editor>
 
         <div class="mt-4 p-3 border rounded border-warning">
-            <h2><i class="fas fa-hammer me-2"></i>Under Construction</h2>
-            This page is under construction even more so than usual, with the following outstanding:
+            <h2><i class="fas fa-hammer me-2"></i>Version 2?</h2>
+            A lot of things came up in re-writing this page, enough so that we're preserving future possibilities.
             <ul>
-                <li>Responsive layout review (e.g. placing hints to the side if there's room rather than beneath, etc).
-                <li>Deletion is only partially implemented (Forms are marked as deleted and hidden)</li>
+                <li>Combine the libraries running this and form browser</li>
                 <li>Strongly considering removing the main preview or making it run by request.</li>
                 <li>Adding a preview by each description to save scrolling around.</li>
+                <li>Add in Flora's error checking as an option</li>
                 <li>Maybe make each area (e.g. head, skin, etc) a separate sub-tab rather than using a giant list.</li>
+                <li>Maybe make it so there's only one editor and switch things in and out of it as required?</li>
                 <li>Not having all the help tips as a dump at the top of the parts&description tab.</li>
                 <li>More links to help files.</li>
-                <li>Find (another) data table library replacement, because this one is slow with large amounts of data.</li>
             </ul>
         </div>
     </div>
