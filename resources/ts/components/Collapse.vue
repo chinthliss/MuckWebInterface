c7bc37a89dd1a74407547537b4fe8a25dc617f29
chinthliss <55845146+chinthliss@users.noreply.github.com>
2025-09-12 16:47:34 +0100

    feat: Add a Collapse component

---
+++ resources/ts/components/Collapse.vue
@@ -0,0 +1,62 @@
+<script lang="ts" setup>
+
+import {ref} from "vue";
+
+defineProps<{
+    title?: string,
+}>();
+
+const expanded = ref<boolean>(false);
+
+const emit = defineEmits(['shown', 'hidden', 'mounted'])
+
+const show = () => {
+    if (expanded.value) return;
+    expanded.value = true;
+    emit('shown');
+}
+
+const hide = () => {
+    if (!expanded.value) return;
+    expanded.value = false;
+    emit('hidden');
+}
+
+const toggle = () => {
+    if (expanded.value) hide(); else show();
+}
+
+defineExpose({show, hide});
+
+</script>
+
+<template>
+    <div id="collapse-toggle" :class="{
+            'rounded-top': expanded,
+            'rounded': !expanded,
+            'border-bottom-0': expanded
+         }" aria-controls="content"
+         class="d-flex align-items-center border-primary-subtle border p-1 "
+         @click="toggle"
+    >
+        <div :aria-expanded="expanded" aria-hidden="true" class="btn btn-primary me-2">
+            <i v-if="expanded" class="fa fa-chevron-up"></i>
+            <i v-else class="fa fa-chevron-down"></i>
+        </div>
+        <div id="content-title">{{ title || 'Collapse' }}</div>
+    </div>
+    <div v-if="expanded" id="content" :class="$attrs.class"
+         aria-labelledby="content-title"
+         class="border-primary-subtle border rounded-bottom bg-body"
+    >
+        <slot>
+            No content provided for collapse
+        </slot>
+    </div>
+</template>
+
+<style scoped>
+#collapse-toggle {
+    cursor: pointer;
+}
+</style>
