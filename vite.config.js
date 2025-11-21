a0b5ab055638be678ebb0c480c938c0c1b26293a
chinthliss <55845146+chinthliss@users.noreply.github.com>
2025-11-21 15:42:04 +0000

    build: Change chunking strategy
    All very moot, since all components are loaded on demand, but leaving room for possible improvement later.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import vue from '@vitejs/plugin-vue';
 import { visualizer } from "rollup-plugin-visualizer";
 
 function manualChunks(id) {
+    // Stringparsing functionality
+    if (id.includes('codemirror') || id.includes('lezer')) {
+        return 'stringparsing';
+    }
+    // Datatables functionality - this is the only module using jquery so gets that too
+    if (id.includes('datatables') || id.includes('jquery')) {
+        return 'datatables';
+    }
+
     if (id.includes('node_modules')) {
         return 'vendor';
     }
